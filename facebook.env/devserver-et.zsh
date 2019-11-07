700edcb4139b49bae324e602ca9675ae29be0c76
Patrick Williams <patrick@stwcx.xyz>
2019-11-06 21:14:37 -0600

    facebook: split devserver connect
    In some cases tmux-attach seems to exit with an error
    even though the session exists, such as when reconnecting
    the 'et' connection.  Split it into two commands.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- facebook.env/devserver-et.zsh
+++ facebook.env/devserver-et.zsh
@@ -2,6 +2,9 @@
 
 if (($DOTFILES_CONFIG[(Ie)macos])); then
     function devserver() {
-        et -c "tmux -CC attach || tmux -CC" -x patrickw3.sb.facebook.com:8080
+        et -c "tmux -CC attach" -x patrickw3.sb.facebook.com:8080
+    }
+    function devserver-launch() {
+        et -c "tmux -CC" -x patrickw3.sb.facebook.com:8080
     }
 fi
