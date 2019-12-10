ee1edf1a5a4bc95d9a1dbc9e858c63bccc2a5d4f
Patrick Williams <patrickw3@fb.com>
2019-12-10 13:27:05 -0800

    facebook: linux: fix proxy config
    Missing 'export' on shell variables.
    Fixes d7ba869d7916ac1b698e4745a0ffdca406b55927
    
    Signed-off-by: Patrick Williams <patrickw3@fb.com>

--- env/55_facebook_linux/devserver-proxy.zsh
+++ env/55_facebook_linux/devserver-proxy.zsh
@@ -3,6 +3,6 @@
 export no_proxy=".fbcdn.net,.facebook.com,.thefacebook.com,.tfbnw.net,.fb.com,.fburl.com,.facebook.net,.sb.fbsbx.com,localhost"
 
 #eval $(fwdproxy-config --format=sh curl | sed "s/^/export /")
-http_proxy=http://fwdproxy:8080
-https_proxy=http://fwdproxy:8080
-ftp_proxy=http://fwdproxy:8080
+export http_proxy=http://fwdproxy:8080
+export https_proxy=http://fwdproxy:8080
+export ftp_proxy=http://fwdproxy:8080
