863842c54be2bb3fcf8a4375353f93ad70aaa1e3
Patrick Williams <patrick@stwcx.xyz>
2020-02-06 11:19:22 -0600

    lf-openbmc: utility to review by maintainer list
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- env/20_all/lfopenbmc.zsh
+++ env/20_all/lfopenbmc.zsh
@@ -8,3 +8,10 @@ function gerrit-origin() {
     local REPONAME=$(git rev-parse --show-toplevel | xargs basename)
     git config remote.origin.url ssh://openbmc.gerrit/openbmc/$REPONAME
 }
+function gerrit-maint() {
+    local REVIEWERS=( $(grep '^[MR]:' \
+        $(git rev-parse --show-toplevel)/MAINTAINERS |
+        sed 's/.*<\([^>]*@[^>]*\)>.*/r=\1/') )
+
+    echo ${(j:,:)REVIEWERS}
+}
