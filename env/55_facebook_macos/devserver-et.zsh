81b8b7748d6eccac9e01f0626000053490013c41
Patrick Williams <patrick@stwcx.xyz>
2020-03-03 09:53:02 -0600

    facebook: devserver: fix login with newer et
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- env/55_facebook_macos/devserver-et.zsh
+++ env/55_facebook_macos/devserver-et.zsh
@@ -2,10 +2,10 @@
 
 function devserver() {
     et -f -t 9999:9999 -x -c "tmux -CC attach" \
-        patrickw3.sb.facebook.com:8080
+        patrickw3@patrickw3.sb.facebook.com:8080
 }
 
 function devserver-launch() {
     et -f -t 9999:9999 -x -c "tmux -CC" \
-        patrickw3.sb.facebook.com:8080
+        patrickw3@patrickw3.sb.facebook.com:8080
 }
