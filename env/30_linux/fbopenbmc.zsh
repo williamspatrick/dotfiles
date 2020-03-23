9589a7a0d94481d5023f3974857e66955a322f60
Patrick Williams <patrick@stwcx.xyz>
2020-03-23 09:04:05 -0500

    fbopenbmc: remove 'fb-only-network' on non-Facebook systems
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- env/30_linux/fbopenbmc.zsh
+++ env/30_linux/fbopenbmc.zsh
@@ -9,8 +9,18 @@ function fb-obmc() {
     wd fbopenbmc &&
     source openbmc-init-build-env $FB_MACHINE \
         ~/local/builds/build-$FB_MACHINE &&
+
+    # Set up SSTATE_DIR
     if ! grep -q "^SSTATE_DIR" conf/local.conf ; then
         echo "SSTATE_DIR ?= \"$HOME/local/builds/sstate-cache\"" >> \
             conf/local.conf
+    fi
+
+    # Remove 'fb-only-network' on non-Facebook systems.
+    if [[ $DOTFILES_SYSTEM != *facebook* ]]; then
+        if ! grep -q "^INHERIT_remove" conf/local.conf ; then
+            echo "INHERIT_remove = \"fb-only-network\"" >> \
+                conf/local.conf
+        fi
     fi
 }
