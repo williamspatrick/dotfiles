4bba43a350af40c3a931b058db43d2dbb2ecd88b
Patrick Williams <patrick@stwcx.xyz>
2020-03-22 21:30:36 -0500

    openbmc: make scripts usable on all linux
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

---
+++ env/30_linux/fbopenbmc.zsh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+function fb-obmc() {
+    if [ -z $1 ]; then
+        echo "Missing machine." && return
+    fi
+    FB_MACHINE=$1
+
+    wd fbopenbmc &&
+    source openbmc-init-build-env $FB_MACHINE \
+        ~/local/builds/build-$FB_MACHINE &&
+    if ! grep -q "^SSTATE_DIR" conf/local.conf ; then
+        echo "SSTATE_DIR ?= \"$HOME/local/builds/sstate-cache\"" >> \
+            conf/local.conf
+    fi
+}
