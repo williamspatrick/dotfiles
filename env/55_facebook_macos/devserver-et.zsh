b7f628a71bc7c687962cdf3e7eaaec560892772d
Patrick Williams <patrick@stwcx.xyz>
2021-04-12 15:08:02 -0500

    et: remove SSH agent forwarding
    Using SSH agent forwarding with et seems to cause silent
    hangs on SSH connection for some systems.  Just turn it
    off since it usually isn't what I want to use anyhow.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- env/55_facebook_macos/devserver-et.zsh
+++ env/55_facebook_macos/devserver-et.zsh
@@ -1,11 +1,11 @@
 #!/usr/bin/env zsh
 
 function devserver() {
-    et -f -t 9999:9999 -x -c "tmux -CC attach" \
+    et -t 9999:9999 -x -c "tmux -CC attach" \
         patrickw3@patrickw3.sb.facebook.com:8080
 }
 
 function devserver-launch() {
-    et -f -t 9999:9999 -x -c "tmux -CC" \
+    et -t 9999:9999 -x -c "tmux -CC" \
         patrickw3@patrickw3.sb.facebook.com:8080
 }
