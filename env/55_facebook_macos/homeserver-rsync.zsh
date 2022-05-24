91aa14eb53d4c98654e0ef10e848f26a5975577e
Patrick Williams <patrick@stwcx.xyz>
2022-05-24 11:31:01 -0500

    macos: rsync: support away-from-home rsync
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- env/55_facebook_macos/homeserver-rsync.zsh
+++ env/55_facebook_macos/homeserver-rsync.zsh
@@ -1,25 +1,33 @@
 #!/usr/bin/env zsh
 
+function home-server-hostname() {
+    if ifconfig -X en | grep "inet 192.168.1.132" > /dev/null ; then
+        echo "192.168.1.32"
+    else
+        echo "home-server"
+    fi
+}
+
 function fbobmc-to-home() {
     wd fbopenbmc
     rsync --delete --delete-after -avr \
-        . apwillia@192.168.1.32:local/sync/fbopenbmc
+        . apwillia@$(home-server-hostname):local/sync/fbopenbmc
 }
 
 function fbobmc-from-home() {
     wd fbopenbmc
     rsync --delete --delete-after -avr \
-        apwillia@192.168.1.32:local/sync/fbopenbmc/ .
+        apwillia@$(home-server-hostname):local/sync/fbopenbmc/ .
 }
 
 function obmcsrc-to-home() {
     wd obmcsrc
     rsync --delete --delete-after -avr \
-        . apwillia@192.168.1.32:local/sync/openbmc-sources
+        . apwillia@$(home-server-hostname):local/sync/openbmc-sources
 }
 
 function obmcsrc-from-home() {
     wd obmcsrc
     rsync --delete --delete-after -avr \
-        apwillia@192.168.1.32:local/sync/openbmc-sources/ .
+        apwillia@$(home-server-hostname):local/sync/openbmc-sources/ .
 }
