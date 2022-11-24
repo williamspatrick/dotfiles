d228f52b406228f999e41ee38f5c44b2e142a3c3
Patrick Williams <patrick@stwcx.xyz>
2022-11-23 18:01:52 -0600

    path: add npm install location
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- env/20_all/private-path.zsh
+++ env/20_all/private-path.zsh
@@ -1,3 +1,10 @@
 #!/usr/bin/env zsh
 
-export PATH=$HOME/.local/bin:$HOME/private/bin:$HOME/.cargo/bin:$PATH
+NPM_PATH=""
+if which npm > /dev/null; then
+    NPM_PATH="$(npm bin):"
+fi
+
+export PATH=$HOME/.local/bin:$HOME/private/bin:$HOME/.cargo/bin:$NPM_PATH$PATH
+
+unset NPM_PATH
