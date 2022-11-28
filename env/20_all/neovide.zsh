09ef36533e6e2bf57355ff79d93e4a7a05dc4c82
Patrick Williams <patrickw3@meta.com>
2022-11-28 15:18:59 -0800

    nvim: fix nv alias for all machines
    Signed-off-by: Patrick Williams <patrickw3@meta.com>

--- env/20_all/neovide.zsh
+++ env/20_all/neovide.zsh
@@ -1,9 +1,8 @@
 #!/usr/bin/env zsh
 
+alias nv="nvim"
 if which neovide > /dev/null; then
     if [ -n "$GDMSESSION" ]; then
         alias nv="neovide"
-    else
-        alias nv="nvim"
     fi
 fi
