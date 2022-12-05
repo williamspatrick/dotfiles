7ececbfa327ec078643191f029cdcfc6f8dd2342
Patrick Williams <patrick@stwcx.xyz>
2022-12-05 06:42:46 -0600

    clang: reorder path to end
    The clang paths add executables like c++ which we do not want to
    override the system ones.  Place the clang paths at the end of PATH.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- env/11_gentoo/clang.zsh
+++ env/11_gentoo/clang.zsh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 
-export PATH=$(grep "^PATH=" /etc/env.d/*llvm*([1]) | sed 's/.*=//' | sed 's/"//g'):$PATH
+export PATH=$PATH:$(grep "^PATH=" /etc/env.d/*llvm*([1]) | sed 's/.*=//' | sed 's/"//g')
