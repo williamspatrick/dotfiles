004e6779c7cd2026f9ed1af5ede4a3a7a10a7f34
Patrick Williams <patrick@stwcx.xyz>
2022-12-06 09:48:07 -0600

    shellcheck: fix issues
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- files/poky_docker/launch.bash
+++ files/poky_docker/launch.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 # Set up symlink from workdir (real home) local.
 ln -s /workdir/local "${HOME}"
@@ -6,10 +7,13 @@ mkdir /tmp/depmod && touch /tmp/depmod/depmod && chmod 755 /tmp/depmod/depmod
 export PATH=${PATH}:/tmp/depmod
 
 function wd() {
-    cd $(grep "$1" /workdir/.warprc | sed 's#.*:~#/workdir#') || exit
+    local DIR
+    DIR="$( (grep "$1" /workdir/.warprc || true) | sed 's#.*:~#/workdir#')"
+    cd "${DIR}" || exit
 }
 
+# shellcheck source=/dev/null
 source /workdir/.zinit/plugins/williamspatrick---dotfiles/env/30_linux/fbopenbmc.zsh
 
 fb-obmc "$1"
-bash --rcfile <(alias)
+bash --rcfile <(alias || true)
