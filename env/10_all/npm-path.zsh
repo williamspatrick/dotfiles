d90c6afac73ec2f41e001f3488b18d2d2aef7110
Patrick Williams <patrickw3@meta.com>
2023-01-23 14:45:43 -0800

    npm: handle weird error on devserver
    Signed-off-by: Patrick Williams <patrickw3@meta.com>

--- env/10_all/npm-path.zsh
+++ env/10_all/npm-path.zsh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 
 if which npm > /dev/null; then
-    export PATH="$(npm bin):$PATH"
-    export NODE_PATH="$(npm root)"
+    if npm bin > /dev/null; then
+        export PATH="$(npm bin):$PATH"
+        export NODE_PATH="$(npm root)"
+    fi
 fi
