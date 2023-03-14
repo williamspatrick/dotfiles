c3ab9b781b3a60fa573b7e3adb8cbaaeb4552ceb
Patrick Williams <patrick@stwcx.xyz>
2023-03-14 09:38:22 -0500

    npm: `npm bin` is removed in 9.x
    Use a relative path off of `npm root`.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- env/10_all/npm-path.zsh
+++ env/10_all/npm-path.zsh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 
 if which npm > /dev/null; then
-    if npm bin > /dev/null; then
-        export PATH="$(npm bin):$PATH"
+    if npm root > /dev/null; then
+        export PATH="$(npm root)/.bin:$PATH"
         export NODE_PATH="$(npm root)"
     fi
 fi
