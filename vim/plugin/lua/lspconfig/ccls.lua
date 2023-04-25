2c3d938e08b4a129dbca4f5e2bfd4ccad7fc9725
Patrick Williams <patrick@stwcx.xyz>
2023-04-25 15:51:36 -0500

    nvim: lsp: ccls: enable C++20
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- vim/plugin/lua/lspconfig/ccls.lua
+++ vim/plugin/lua/lspconfig/ccls.lua
@@ -1,6 +1,9 @@
 require("lspconfig").ccls.setup({
     init_options = {
         compilationDatabaseDirectory = vim.g.meson_build_dir,
+        clang = {
+            extraArgs = { "-std=c++20" },
+        },
     },
     capabilities = require("cmp_nvim_lsp").default_capabilities(),
 })
