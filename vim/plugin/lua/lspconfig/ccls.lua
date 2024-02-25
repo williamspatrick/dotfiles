427d145c5eb04b4255c6ae0982b5025b62195b8e
Patrick Williams <patrick@stwcx.xyz>
2024-02-25 16:33:20 -0600

    nvim: ccls: update C++ standard to c++23
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- vim/plugin/lua/lspconfig/ccls.lua
+++ vim/plugin/lua/lspconfig/ccls.lua
@@ -2,7 +2,7 @@ require("lspconfig").ccls.setup({
     init_options = {
         compilationDatabaseDirectory = vim.g.meson_build_dir,
         clang = {
-            extraArgs = { "-std=c++20" },
+            extraArgs = { "-std=c++23" },
         },
     },
     capabilities = require("cmp_nvim_lsp").default_capabilities(),
