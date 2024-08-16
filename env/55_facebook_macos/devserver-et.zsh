cec703a5ad4d5311d34da1d2e513fd9e91742d72
Patrick Williams <patrick@stwcx.xyz>
2024-08-16 10:23:23 -0400

    et: use x2ssh for better connectivity
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- env/55_facebook_macos/devserver-et.zsh
+++ env/55_facebook_macos/devserver-et.zsh
@@ -1,11 +1,13 @@
 #!/usr/bin/env zsh
 
 function devserver() {
-    et -t 9999:9999 -x -c "tmux -CC attach" \
-        patrickw3.sb.facebook.com:8080
+    x2ssh -et patrickw3.sb.facebook.com -c "tmux -CC attach"
+    # et -t 9999:9999 -x -c "tmux -CC attach" \
+    #    patrickw3.sb.facebook.com:8080
 }
 
 function devserver-launch() {
-    et -t 9999:9999 -x -c "tmux -CC" \
-        patrickw3.sb.facebook.com:8080
+    x2ssh -et patrickw3.sb.facebook.com -c "tmux -CC"
+    # et -t 9999:9999 -x -c "tmux -CC" \
+    #    patrickw3.sb.facebook.com:8080
 }
