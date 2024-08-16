c837f4626a37a129d55c0016ecfa8d6c143c500d
Patrick Williams <patrick@stwcx.xyz>
2024-08-16 10:22:58 -0400

    rsync: add progress to homeserver connection
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- env/55_facebook_macos/homeserver-rsync.zsh
+++ env/55_facebook_macos/homeserver-rsync.zsh
@@ -2,24 +2,24 @@
 
 function fbobmc-to-home() {
     wd fbopenbmc
-    rsync --delete --delete-after -avr \
+    rsync --delete --delete-after -avr --progress \
         . apwillia@$(home-server-hostname):local/sync/fbopenbmc
 }
 
 function fbobmc-from-home() {
     wd fbopenbmc
-    rsync --delete --delete-after -avr \
+    rsync --delete --delete-after -avr --progress \
         apwillia@$(home-server-hostname):local/sync/fbopenbmc/ .
 }
 
 function obmcsrc-to-home() {
     wd obmcsrc
-    rsync --delete --delete-after -avr \
+    rsync --delete --delete-after -avr --progress \
         . apwillia@$(home-server-hostname):local/sync/openbmc-sources
 }
 
 function obmcsrc-from-home() {
     wd obmcsrc
-    rsync --delete --delete-after -avr \
+    rsync --delete --delete-after -avr --progress \
         apwillia@$(home-server-hostname):local/sync/openbmc-sources/ .
 }
