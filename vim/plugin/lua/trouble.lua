e569cadc8a421de63430afc5f77512def6f6a592
Patrick Williams <patrick@stwcx.xyz>
2024-10-02 16:14:34 -0400

    nvim: trouble: adjust toggle command
    Trouble did a rewrite and "TroubleToggle" doesn't exist anymore.
    Use "Trouble diagnostics toggle" instead.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- vim/plugin/lua/trouble.lua
+++ vim/plugin/lua/trouble.lua
@@ -1,3 +1,3 @@
 require("trouble").setup()
 
-vim.keymap.set({ "i", "n", "v" }, "<C-`>", "<cmd>TroubleToggle<cr>", {})
+vim.keymap.set({ "i", "n", "v" }, "<C-`>", "<cmd>Trouble diagnostics toggle<cr>", {})
