43d4f9f158b147f80a26773831aa157e9e8aa4e7
Patrick Williams <patrick@stwcx.xyz>
2024-10-18 18:50:22 -0400

    telescope: tweak layout and add new keybindings
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- vim/plugin/lua/telescope.lua
+++ vim/plugin/lua/telescope.lua
@@ -1,6 +1,13 @@
+require("telescope").setup({
+    defaults = {
+        layout_strategy= "flex",
+    }
+})
 local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>ff", builtin.git_files, {})
+vim.keymap.set("n", "<leader>fv", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fs", builtin.treesitter, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fd", builtin.lsp_definitions, {})
 vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
