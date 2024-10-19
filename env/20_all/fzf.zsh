2f0b8042265813eeef0a44fd9cc71d33beaad7f5
Patrick Williams <patrick@stwcx.xyz>
2024-10-18 20:34:25 -0400

    zsh: add fzf options
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

---
+++ env/20_all/fzf.zsh
@@ -0,0 +1,14 @@
+if which fzf > /dev/null; then
+    FZF_ALT_C_COMMAND=
+    source <(fzf --zsh)
+
+    if which rg > /dev/null; then
+        FZF_DEFAULT_COMMAND='rg --files --hidden --glob "!.git/*"'
+        FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
+    fi
+
+    if which bat > /dev/null; then
+        FZF_DEFAULT_OPTS="--preview='bat --color=always --line-range:200 {}' --preview-window='right,50%,<50(down:10)' --layout=reverse --height=50%"
+        FZF_CTRL_R_OPTS="--no-preview"
+    fi
+fi
