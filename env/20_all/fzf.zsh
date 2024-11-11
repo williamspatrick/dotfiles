681670a403d91d6422914f181b077d91cb86ea73
Patrick Williams <patrick@stwcx.xyz>
2024-11-11 15:54:33 -0500

    fzf: adjust 'bat' args
    `bat` expects a space after `--line-range` on certain versions.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- env/20_all/fzf.zsh
+++ env/20_all/fzf.zsh
@@ -8,7 +8,7 @@ if which fzf > /dev/null; then
     fi
 
     if which bat > /dev/null; then
-        FZF_DEFAULT_OPTS="--preview='bat --color=always --line-range:200 {}' --preview-window='right,50%,<50(down:10)' --layout=reverse --height=50%"
+        FZF_DEFAULT_OPTS="--preview='bat --color=always --line-range :200 {}' --preview-window='right,50%,<50(down:10)' --layout=reverse --height=50%"
         FZF_CTRL_R_OPTS="--no-preview"
     fi
 fi
