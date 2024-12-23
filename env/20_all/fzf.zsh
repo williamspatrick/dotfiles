0e34986d450b03883726832833472e8fbe46528e
Patrick Williams <patrick@stwcx.xyz>
2024-12-23 16:49:18 -0500

    fzf: use `fd` color output
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- env/20_all/fzf.zsh
+++ env/20_all/fzf.zsh
@@ -1,7 +1,7 @@
 if which fzf > /dev/null; then
 
     if which fd > /dev/null; then
-        FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git'
+        FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git --color=always'
         FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
     elif which rg > /dev/null; then
         FZF_DEFAULT_COMMAND='rg --files --hidden --glob "!.git/*"'
@@ -9,7 +9,7 @@ if which fzf > /dev/null; then
     fi
 
     if which bat > /dev/null; then
-        FZF_DEFAULT_OPTS="--preview='bat --color=always --line-range :200 {}' --preview-window='right,50%,<50(down:10)' --layout=reverse --height=50%"
+        FZF_DEFAULT_OPTS="--ansi --preview='bat --color=always --line-range :200 {}' --preview-window='right,50%,<50(down:10)' --layout=reverse --height=50%"
         FZF_CTRL_R_OPTS="--no-preview"
         FZF_ALT_C_OPTS="--no-preview"
     fi
