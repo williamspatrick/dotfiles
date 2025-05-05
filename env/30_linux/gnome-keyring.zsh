1cefaa3ec060adfdbc2e96fd2d7bc2664e5803d8
Patrick Williams <patrick@stwcx.xyz>
2025-05-05 15:54:31 -0400

    gnome-keyring: use gcr agent
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- env/30_linux/gnome-keyring.zsh
+++ env/30_linux/gnome-keyring.zsh
@@ -1,7 +1,6 @@
 # Set up gnome-keyring as necessary.
 case $XDG_SESSION_DESKTOP in
     sway)
-        eval $(gnome-keyring-daemon --start 2>/dev/null)
-        export SSH_AUTH_SOCK
+        export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/gcr/ssh
         ;;
 esac
