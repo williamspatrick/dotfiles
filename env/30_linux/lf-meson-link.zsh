f225c0bc6124478cf8871649a8f0c2bef0520b06
Patrick Williams <patrick@stwcx.xyz>
2025-12-02 19:52:42 -0500

    lf-meson-link: make more portable
    Allow the script to be used from any directory by leveraging `wd` to
    find the candidate link target.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- env/30_linux/lf-meson-link.zsh
+++ env/30_linux/lf-meson-link.zsh
@@ -4,11 +4,22 @@ function lf-meson-link() {
     FILES=($(ls *.wrap | sed 's/\.wrap//'))
     for f in ${FILES[@]}
     do
-        realpath "../../$f"
-        if [ ! -d "../../$f" ];
+        if ! grep -q "revision = HEAD" $f.wrap ; then
+            echo "skipping $f: not a HEAD revision"
+            continue
+        fi
+
+        realdir="$(realpath $(wd path obmcsrc)/$f)"
+        if [ ! -d $realdir ];
+        then
+            echo "skipping $f: does not exist"
+            continue
+        fi
+
+        if [ -e $f ];
         then
             continue
         fi
-        ln -s ../../$f $f
+        ln -s $realdir $f
     done
 }
