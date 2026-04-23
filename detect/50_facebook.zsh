891ebbeb55fda931a294f6f75941b99ffab9a9f7
Patrick Williams <patrick@stwcx.xyz>
2026-04-23 16:02:10 -0700

    facebook: detect: add on-demand detection
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- detect/50_facebook.zsh
+++ detect/50_facebook.zsh
@@ -2,4 +2,7 @@
 
 if [[ $(whoami) == "patrickw3" ]]; then
     DOTFILES_CONFIG=("${DOTFILES_CONFIG[@]}" "facebook")
+    if [ -e /etc/ondemand-whoami ]; then
+        DOTFILES_CONFIG=("${DOTFILES_CONFIG[@]}" "ondemand")
+    fi
 fi
